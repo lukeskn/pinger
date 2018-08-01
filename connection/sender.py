95850b9ea650679fc0d0e6576e8c4102bb88f49f
root <root@vps.server.com>
2018-08-01 03:33:34 -0400

    added custom message support

--- connection/sender.py
+++ connection/sender.py
@@ -2,6 +2,7 @@ from other import validate
 import socket
 import time
 import sys
+from other import generalInformation as utils
 NOT_SET = -1
 SECOND = 1
 DEFAULT_SPEED = 1
@@ -9,19 +10,24 @@ DEFAULT_SPEED = 1
 class SenderClient(object):
 
     # Initializes the Sender Object
-    def __init__(self, connectionType, port, ip, repeat, repeatsSecond, messageSize):
+    def __init__(self, connectionType, port, ip, repeat, repeatsSecond, messageSize, message, file):
         self.connectionType = connectionType
         self.port = port
         self.ip = ip
         self.repeat = repeat
         self.messageSize = messageSize
-        self.message = ""
+        self.message = message
         self.repeatsSecond = repeatsSecond
+        self.file = file
         self.startSender()
 
     # Checks if all important Information are given and tries to start the TCP or UDP sender.
     def startSender(self):
-        self.message = self.generateMessage()
+        if self.file != NOT_SET:
+            self.message, self.repeat = utils.importFile(self.file)
+        else:
+            if self.message == NOT_SET:
+                self.message = self.generateMessage()
         if validate.allInformationGiven(self.connectionType, self.ip, self.port):
             if self.connectionType == 1:
                 print("TCP Sender successfully started.")
@@ -51,10 +57,11 @@ class SenderClient(object):
     # Starts the UDP sender.
     def doUDPSender(self):
 
-        def doUDPConnection():
+        def doUDPConnection(message):
+
             time.sleep(self.getSleeptimefromRepeats())
             start = time.time()
-            sock.sendto(self.message.encode(), addr)
+            sock.sendto(message.encode("utf-8"), addr)
             try:
                 data, server = sock.recvfrom(1024)
                 if data:
@@ -72,14 +79,17 @@ class SenderClient(object):
 
         if self.repeat == NOT_SET:
             while True:
-                doUDPConnection()
+                doUDPConnection(self.message)
         else:
             for counter in range(int(self.repeat)):
-                doUDPConnection()
+                if self.file == NOT_SET:
+                    doUDPConnection(self.message)
+                else:
+                    doUDPConnection(self.message[counter])
 
     # Starts the TCP sender.
     def doTCPSender(self):
-        def doTCPConnection():
+        def doTCPConnection(message):
             try:
                 time.sleep(self.getSleeptimefromRepeats())
                 sock = socket.socket(socket.AF_INET)
@@ -87,7 +97,7 @@ class SenderClient(object):
 
                 sock.connect((self.ip, int(self.port)))
                 start = time.time()
-                sock.sendall(self.message.encode('utf-8'))
+                sock.sendall(message.encode('utf-8'))
                 data = sock.recv(1024)
                 end = time.time()
                 sock.close()
@@ -99,7 +109,10 @@ class SenderClient(object):
 
         if self.repeat == NOT_SET:
             while True:
-                doTCPConnection()
+                doTCPConnection(self.message)
         else:
             for counter in range(int(self.repeat)):
-                doTCPConnection()
+                if self.file == NOT_SET:
+                    doTCPConnection(self.message)
+                else:
+                    doTCPConnection(self.message[counter])
