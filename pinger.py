b04cf32f99d76c1ba03505deaf9de7cba1721953
root <root@vps.server.com>
2018-08-07 02:34:16 -0400

    checks for direct execution

--- pinger.py
+++ pinger.py
@@ -3,7 +3,7 @@ from connection import sender as sender, receiver as receiver
 import re
 from other import generalInformation, validate
 
-VERSION = 2.35
+VERSION = 2.353
 
 TCP = 1
 UDP = 2
@@ -74,74 +74,76 @@ def splitParam(param):
 # Returns all digits from a given String.
 def removeCharacterFromString(string):
     return re.findall('\d+', string)
-try:
-    # Read Start Parameter
-    if len(sys.argv) > 1:
-        if isQuickStart():
-            connectionType, ip, port = initQuickStart()
-        lastParam = ""
-        for i in range(1,len(sys.argv)):
-            param = validateParam(sys.argv[i])
-            if param == "help" or param == "\?" or param == "?" or param == "/?":
-                generalInformation.sendHelpInformation(VERSION)
-                sys.exit(0)
-            if param == "tcp":
-                connectionType = TCP
-            elif param == "udp":
-                connectionType = UDP
-            elif param[0] == "l":
-                try:
-                    messageSize = removeCharacterFromString(sys.argv[i])
-                    messageSize = int(messageSize[0])
-                except IndexError:
-                    messageSize = NOT_SET
-            elif param[:2] == "$a":
-                try:
-                    message = sys.argv[i]
-                    message = message[3:]
-                except Exception:
-                    message = NOT_SET
-            elif param[:2] == "$f":
-                try:
-                    file = sys.argv[i]
-                    file = file[3:]
-                except Exception:
-                    file = NOT_SET
-            elif param == "cip":
-                generalInformation.printIPs()
-                sys.exit(0)
-            elif param[:2] == "re":
-                try:
-                    repeatsSecond = removeCharacterFromString(sys.argv[i])
-                    repeatsSecond = int(repeatsSecond[0])
-                except IndexError:
-                    repeatsSecond = NOT_SET
-            elif param[:4] == "port" or param[0] == "p":
-                try:
-                    port = removeCharacterFromString(sys.argv[i])
-                    port = int(port[0])
-                except IndexError:
-                    port = NOT_SET
-            elif param[:2] == "ip":
-                try:
-                    ip = sys.argv[i]
-                    ip = re.search("\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}$", ip).group()
-                except IndexError:
-                    ip = NOT_SET
-            elif param[0] == "t":
-                try:
-                    repeat = removeCharacterFromString(sys.argv[i])
-                    repeat = int(repeat[0])
-                except IndexError:
-                    repeat = NOT_SET
-            elif param == "r" or param == "receiver":
-                clientType = RECEIVER
-        if clientType != NOT_SET:
-            if(clientType == 1):
-                startSender()
-            else:
-                startReceiver()
-    else:
-        noParamAtStart()
-except KeyboardInterrupt:
-    sys.exit(0)
+
+if __name__ == "__main__":
+    try:
+        # Read Start Parameter
+        if len(sys.argv) > 1:
+            if isQuickStart():
+                connectionType, ip, port = initQuickStart()
+            lastParam = ""
+            for i in range(1,len(sys.argv)):
+                param = validateParam(sys.argv[i])
+                if param == "help" or param == "\?" or param == "?" or param == "/?":
+                    generalInformation.sendHelpInformation(VERSION)
+                    sys.exit(0)
+                if param == "tcp":
+                    connectionType = TCP
+                elif param == "udp":
+                    connectionType = UDP
+                elif param[0] == "l":
+                    try:
+                        messageSize = removeCharacterFromString(sys.argv[i])
+                        messageSize = int(messageSize[0])
+                    except IndexError:
+                        messageSize = NOT_SET
+                elif param[:2] == "$a":
+                    try:
+                        message = sys.argv[i]
+                        message = message[3:]
+                    except Exception:
+                        message = NOT_SET
+                elif param[:2] == "$f":
+                    try:
+                        file = sys.argv[i]
+                        file = file[3:]
+                    except Exception:
+                        file = NOT_SET
+                elif param == "cip":
+                    generalInformation.printIPs()
+                    sys.exit(0)
+                elif param[:2] == "re":
+                    try:
+                        repeatsSecond = removeCharacterFromString(sys.argv[i])
+                        repeatsSecond = int(repeatsSecond[0])
+                    except IndexError:
+                        repeatsSecond = NOT_SET
+                elif param[:4] == "port" or param[0] == "p":
+                    try:
+                        port = removeCharacterFromString(sys.argv[i])
+                        port = int(port[0])
+                    except IndexError:
+                        port = NOT_SET
+                elif param[:2] == "ip":
+                    try:
+                        ip = sys.argv[i]
+                        ip = re.search("\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}$", ip).group()
+                    except IndexError:
+                        ip = NOT_SET
+                elif param[0] == "t":
+                    try:
+                        repeat = removeCharacterFromString(sys.argv[i])
+                        repeat = int(repeat[0])
+                    except IndexError:
+                        repeat = NOT_SET
+                elif param == "r" or param == "receiver":
+                    clientType = RECEIVER
+            if clientType != NOT_SET:
+                if(clientType == 1):
+                    startSender()
+                else:
+                    startReceiver()
+        else:
+            noParamAtStart()
+    except KeyboardInterrupt:
+        sys.exit(0)
